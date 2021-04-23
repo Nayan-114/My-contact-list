272c6c57821b418e8df603df7245bf1e83a5a621
Nayan Jyoti Bhuyan <n.bhuyan2014@gmail.com>
2021-04-23 22:05:04 +0530

    Add increaseQuantity function

--- Desktop/React/cart/src/CartItem.js
+++ Desktop/React/cart/src/CartItem.js
@@ -10,6 +10,12 @@ class CartItem extends React.Component{
             qty:1,
             img:''
         }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    // To bind the value,use arrow function,it automatically binds the value
+    increaseQuantity = ()=>
+    {
+        console.log('this',this.state);
     }
 
     render(){
@@ -25,9 +31,22 @@ class CartItem extends React.Component{
                     <div style={{color:'#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
-                        <img alt="Increase" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1619193959~hmac=54e997988f986897aa1a4c777386876f" />
-                        <img alt="Decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1619193864~hmac=3b8b79161a161d32d32a7c62a31d2749" />
-                        <img alt="Delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1619194044~hmac=d1c78c9f44e6d772e6584c47a866502e" />
+                        <img 
+                        alt="Increase" 
+                        className="action-icons" 
+                        src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1619193959~hmac=54e997988f986897aa1a4c777386876f" 
+                        onClick={this.increaseQuantity}
+                        />
+                        <img 
+                        alt="Decrease" 
+                        className="action-icons" 
+                        src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1619193864~hmac=3b8b79161a161d32d32a7c62a31d2749" 
+                        />
+                        <img 
+                        alt="Delete" 
+                        className="action-icons" 
+                        src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1619194044~hmac=d1c78c9f44e6d772e6584c47a866502e" 
+                        />
                     </div>
                 </div>
             </div>
