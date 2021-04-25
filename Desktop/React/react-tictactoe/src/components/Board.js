7a917920d900612ce965c0a1abd2b647658e7ea4
Nayan Jyoti Bhuyan <n.bhuyan2014@gmail.com>
2021-04-25 08:51:43 +0530

    square and board styling

--- Desktop/React/react-tictactoe/src/components/Board.js
+++ Desktop/React/react-tictactoe/src/components/Board.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Square from './Square';
 
-
+const style = {
+    boder:'4px solid dark blue',
+    borderRadius:'10px',
+    width:'250px',
+    height:'250px',
+    margin:'0 auto',
+    display:'grid',
+    gridTemplate:'repeat(3,1fr) / repeat(3,1fr)'
+}
 
 const Board = ({squares,onClick}) => (
-    <div>
+    <div style={style}>
         <Square value="1" onClick={() => onClick("Dummy value")} />
         <Square value="2" onClick={() => onClick("Dummy value")} />
         <Square value="3" onClick={() => onClick("Dummy value")} />
